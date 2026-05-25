abd9f1eb82ef2b2ae08931828ba62e5c59f19eeb
Yhorx <jorge18xdd@gmail.com>
2026-05-25 18:12:07 -0500

    --fix-environments

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SharedModule } from '../shared/shared.module';
-
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-
 
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
-
-
 @NgModule({
   declarations: [
     SidebarComponent,
@@ -18,4 +16,5 @@ import { LazyImageComponent } from './components/lazy-image/lazy-image.component
     LazyImageComponent
   ]
 })
+
 export class SharedModule { }
